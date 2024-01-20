c7b39058c529a6ecd3192e91192c2c61671f2666
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-20 19:00:13 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,10 +1,14 @@
-/*
- * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
- * This is known as the Triangle Inequality Theorem. 
- * Write a program that tests if three given integers form a triangle.
- */ 
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args){
+     int a = Integer.parseInt(args[0]);
+     int b = Integer.parseInt(args[1]);
+     int c = Integer.parseInt(args[2]);
+        // I want that all the different some of two of the side is less than the third one. then I use the sign AND
+     while ( a+b<c || a+c<b || b+c<a) {
+        System.out.println(a + ", " + b + ", " + c + ": false");
+        return;
+     }
+     System.out.println(a + ", " + b + ", " + c + ": true");
+
+    }
 }
