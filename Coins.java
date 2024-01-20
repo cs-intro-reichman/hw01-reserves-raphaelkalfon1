76c0c94f5c95b2c83d638cdac00590c692b805e9
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-20 18:58:51 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -1,9 +1,18 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
 public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
-	}
+    public static void main(String[] args) {
+        int y = 25;
+        int x = 1;
+        
+        int v = Integer.parseInt(args[0]);
+        
+        // k equal to the entire part of the division.
+        int k = v / y;
+
+        // z equal v modulo y, so that it gives me the rest of the division of v/y.
+        int z = v % y;
+        // here, x=1, so it is the same but if x=2 cents then z will not be v modulo y.
+        z = z/x;
+
+        System.out.println("Use " + k + " quarters and " + z + " cents");
+    }
 }
